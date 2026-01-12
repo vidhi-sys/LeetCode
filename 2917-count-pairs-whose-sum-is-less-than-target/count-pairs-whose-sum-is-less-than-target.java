db7c21281610785642f22d12c3c87f082969635f
vidhi udasi <udasividhi2@gmail.com>
2026-01-12 10:43:36 +0530

    Time: 5 ms (3.31%) | Memory: 44.7 MB (7.67%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -1,5 +1,6 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
+        
         int n = nums.size();
         int[] arr = new int[n];
 
@@ -7,19 +8,18 @@ class Solution {
         for (int i = 0; i < n; i++) {
             arr[i] = nums.get(i);
         }
-
-        // Sort the array
         Arrays.sort(arr);
+
+        
 
         int i = 0, j = n - 1;
         int count = 0;
 
         while (i < j) {
             if (arr[i] + arr[j] < target) {
-                // all pairs (i, i+1 ... j) are valid
-                count += (j - i);
+                count+=j-i;
                 i++;
-            } else {
+            } else{
                 j--;
             }
         }
