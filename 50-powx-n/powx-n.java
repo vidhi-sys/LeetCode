0790f2537c25f7c439ce92abd7202e1ef2e51947
vidhi udasi <udasividhi2@gmail.com>
2026-05-22 19:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (71.29%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N = n;
         
+        // handle negative power
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1;
+
+        while (N > 0) {
+            // if odd power
+            if ((N & 1) == 1) {
+                ans *= x;
+            }
+
+            x *= x;
+            N /= 2;
+        }
+
+        return ans;
     }
 }
