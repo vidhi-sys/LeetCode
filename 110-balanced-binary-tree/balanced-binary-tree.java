97b21fa4fdd24887e502d90e11c18932f2f75cbc
vidhi udasi <udasividhi2@gmail.com>
2026-06-06 22:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (40.12%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int height_tree(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int l=height_tree(root.left);
+        int r=height_tree(root.right);
+        if(l==-1||r==-1||Math.abs(l-r)>1){
+            return -1;
+        }
+        return 1+(Math.max(l,r));
+
+    }
+    public boolean isBalanced(TreeNode root) {
+        return height_tree(root)>=0;
+        
+    }
+}
