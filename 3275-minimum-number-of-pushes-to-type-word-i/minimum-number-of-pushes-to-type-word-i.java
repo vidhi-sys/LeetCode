7658f76304c9f6c766a5c29ba90c1dc83ee7a025
vidhi udasi <udasividhi2@gmail.com>
2026-07-30 10:07:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (66.51%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimumPushes(String word) {
+          int q = word.length() >> 3;
+        int r = word.length() & 7;
+        return ((q << 2) + r) * (q + 1);
+
+        
+    }
+}
