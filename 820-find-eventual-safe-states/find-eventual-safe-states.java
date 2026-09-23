571e7afaa69e25c6556f159d633981b57d4a8601
vidhi udasi <udasividhi2@gmail.com>
2026-09-23 11:13:02 +0530

    Time: 26 ms (36.20%) | Memory: 61.9 MB (56.46%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,26 +1,35 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<Integer> res = new ArrayList<>();
-        if(graph == null || graph.length == 0)  return res;
-        
-        int nodeCount = graph.length;
-        int[] color = new int[nodeCount];
-        
-        for(int i = 0;i < nodeCount;i++){
-            if(dfs(graph, i, color))    res.add(i);
+        int n= graph.length;
+        List<List<Integer>> rev = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        int[] indegree= new int[n];
+        for(int i=0;i<n;i++){
+            rev.add(new ArrayList<>());
         }
-        
-        return res;
-    }
-    public boolean dfs(int[][] graph, int start, int[] color){
-        if(color[start] != 0)   return color[start] == 1;
-        
-        color[start] = 2;
-        for(int newNode : graph[start]){
-            if(!dfs(graph, newNode, color))    return false;
+
+        for(int i=0;i<n;i++){
+            for(int ele:graph[i]){
+                rev.get(ele).add(i);
+                indegree[i]++;
+            }
         }
-        color[start] = 1;
-        
-        return true;
-    }
-}
+        Queue<Integer> q= new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(q.size()>0){
+            int top=q.remove();
+            ans.add(top);
+            for(int ele: rev.get(top)){
+                indegree[ele]--;
+                if(indegree[ele]==0){
+                    q.add(ele);
+                }
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }}
